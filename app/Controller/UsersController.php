09c43c4440726745fbb9bc3322e9dc51d0e2ff1d
bigplants <hirakatad@isao.co.jp>
2014-04-07 08:33:26 +0900

    レンダリングが遅いページのテスト #8

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -112,4 +112,9 @@ class UsersController extends AppController
         }
         return $this->redirect(array('action' => 'index'));
     }
+
+    public function page_load_test()
+    {
+
+    }
 }
